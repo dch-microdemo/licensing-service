f63a3ae1ed92a7ebf7797486cc609e767180d785
diegochavezcarro <diegochavezcarro@gmail.com>
2019-05-29 17:51:31 -0300

    Authentication JWT

--- src/main/java/com/diego/microdemo/licenses/LicensingServiceApplication.java
+++ src/main/java/com/diego/microdemo/licenses/LicensingServiceApplication.java
@@ -1,18 +1,33 @@
 package com.diego.microdemo.licenses;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.client.RestTemplate;
 
+import com.diego.microdemo.licenses.utils.UserContextInterceptor;
+
 @SpringBootApplication
+@EnableResourceServer
 public class LicensingServiceApplication {
-	
-    @Bean
-    public RestTemplate getCustomRestTemplate() {
-        RestTemplate template = new RestTemplate();
-        return template;
-    }
+
+	@Bean
+	public RestTemplate getCustomRestTemplate() {
+		RestTemplate template = new RestTemplate();
+		List<ClientHttpRequestInterceptor> interceptors = template.getInterceptors();
+		if (interceptors == null) {
+			template.setInterceptors(Collections.singletonList(new UserContextInterceptor()));
+		} else {
+			interceptors.add(new UserContextInterceptor());
+			template.setInterceptors(interceptors);
+		}
+		return template;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LicensingServiceApplication.class, args);
---
+++ src/main/java/com/diego/microdemo/licenses/utils/UserContext.java
@@ -0,0 +1,16 @@
+package com.diego.microdemo.licenses.utils;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UserContext {
+    public static final String AUTH_TOKEN     = "Authorization";
+
+    private static final ThreadLocal<String> authToken= new ThreadLocal<String>();
+
+    public static String getAuthToken() { return authToken.get(); }
+    public static void setAuthToken(String aToken) {authToken.set(aToken);}
+
+
+}
