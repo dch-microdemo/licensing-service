5b83e4baabb54114b2817ec805c4bf27dbe9ce21
diegochavezcarro <diegochavezcarro@gmail.com>
2019-06-17 14:24:19 -0300

    Kafka consumer andando, clase a deserializar debe ser la misma q fue serializada

---
+++ src/main/java/com/diego/microdemo/licenses/events/services/Consumer.java
@@ -0,0 +1,44 @@
+package com.diego.microdemo.licenses.events.services;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import com.diego.microdemo.organization.events.model.OrganizationChangeModel;
+
+import org.springframework.messaging.handler.annotation.Payload;
+
+@Service
+public class Consumer {
+	private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+
+	@KafkaListener(topics = "orgChangeTopic", groupId = "licensingGroup")
+	public void receiveOrgChange(@Payload OrganizationChangeModel orgChange) {
+		logger.info(String.format("$$ -> Consumed Message with Action -> %s", orgChange.getAction()));
+		logger.debug("Received a message of type " + orgChange.getType());
+		switch (orgChange.getAction()) {
+		case "GET":
+			logger.debug("Received a GET event from the organization service for organization id {}",
+					orgChange.getOrganizationId());
+			break;
+		case "SAVE":
+			logger.debug("Received a SAVE event from the organization service for organization id {}",
+					orgChange.getOrganizationId());
+			break;
+		case "UPDATE":
+			logger.debug("Received a UPDATE event from the organization service for organization id {}",
+					orgChange.getOrganizationId());
+			// organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
+			break;
+		case "DELETE":
+			logger.debug("Received a DELETE event from the organization service for organization id {}",
+					orgChange.getOrganizationId());
+			// organizationRedisRepository.deleteOrganization(orgChange.getOrganizationId());
+			break;
+		default:
+			logger.error("Received an UNKNOWN event from the organization service of type {}", orgChange.getType());
+			break;
+		}
+	}
+}
